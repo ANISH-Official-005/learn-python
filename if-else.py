1b7f5af3c88714fe889fdaceb67bae9f16833cf8
ANISH-Official-005 <anishmani42@gmail.com>
2025-06-19 15:06:42 +0530

    here the

---
+++ if-else.py
@@ -0,0 +1,13 @@
+""" if (False):
+    print("yes")
+else:
+    print("no") """
+""" 
+print('win'=='wina') """
+
+
+rcb="win"
+if(rcb=="win"):
+    print("Ee Sala Cup Namade")
+else:
+    print("lollipop")
