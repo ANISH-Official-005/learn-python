09c1a50b7b4478f3f8d92cd835c7b8e6f7452f63
ANISH-Official-005 <anishmani42@gmail.com>
2025-06-22 16:58:17 +0530

    here the upto question is updated

--- if-else.py
+++ if-else.py
@@ -14,8 +14,21 @@ else:
 
 
 
-meghna=input()
+""" meghna=input()
 if(meghna=="Died"):
     print("surya meets priya")
 else:
-    print("surya weds meghna")
+    print("surya weds meghna") """
+
+""" mark=int(input("enter the mark:"))
+if(mark>35):
+    print("pass")
+else:
+    print("fail") """
+""" 
+
+income=int(input("enter the income:"))
+if(income>7000):
+    print("scholarship is available")
+else:
+    print("not eligible") """
