fb659eca1658aa98351dc31ee15d830d4343caf9
ANISH-Official-005 <anishmani42@gmail.com>
2025-06-27 21:35:44 +0530

    here the for loop code is updated

--- forloop.py
+++ forloop.py
@@ -1,3 +1,5 @@
+# introductionn of for loop
+
 """ for i in range(1,11):
     print(i,"x2=", i*2)
  """
@@ -8,7 +10,30 @@ print(a,b) """
 
 
 
-a=int(input("enter a:"))
+""" a=int(input("enter a:"))
 b=int(input("enter b:"))
 for i in range(a+1,b):
-    print(i)
+    print(i) """
+
+
+# counting concept
+#count the odd numbers between 1 and 10
+""" count=0
+for i in range(1,11):
+    if(i%2!=0):
+        count=count+1
+print("count:",count) """
+
+
+# count the even and odd numbers between 1 and 10
+count_even=0
+count_odd=0
+for i in range(1,11):
+    if(i%2==0):
+        count_even=count_even+1
+    elif(i%2!=0):
+        count_odd=count_odd+1
+    else:
+        print("no count")
+print("even:",count_even)
+print("odd:",count_odd)
