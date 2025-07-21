5e0a10c4cee3f526b238e0c936def38c0e302c84
ANISH-Official-005 <anishmani42@gmail.com>
2025-07-21 10:42:22 +0530

    added upto the return of function

---
+++ return.py
@@ -0,0 +1,37 @@
+#1)Set S_username='EMC' S_password='123' Get input for uname and password Create a function 
+#called validate. If uname and password matches the  function should return true else false.
+""" def validate():
+
+    if(uname=="EMC" and password=="123"):
+        return True
+    else:
+        return False
+
+
+uname=input("enter the uname:")
+password=input("enter the password:")
+S_username="EMC"
+S_password="123"
+result=validate()
+print(result) """
+
+#2.(a+b)*c
+#Get input for a and b and function called add() which should return the
+#sum of a and b
+#And multiply that sum with c
+
+
+
+""" def add():
+       return    
+
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+sum=add()
+print("sum:",sum)
+c=int(input("enter c:"))
+mul=sum*c
+print(mul) """
+
+
