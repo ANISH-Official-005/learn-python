2803610f0cd161340df62e9d080c4aa41e3b9411
ANISH-Official-005 <anishmani42@gmail.com>
2025-07-27 17:15:16 +0530

    till the understanding of constructor

---
+++ costructor and selfkeyword.py
@@ -0,0 +1,35 @@
+#here the __init__ is a inbuilt function and it is a constructor 
+#it automatically get worked wnen a object is just entered into the class
+""" class laptop():
+    def __init__(self):
+        self.price=0
+        self.gb="8gb"
+    def display(self):
+        print("display")
+
+hp=laptop()
+print(hp.price) """
+
+
+
+
+#understanding the working of constructor
+""" class laptop():
+    def __init__(self):
+        self.ram=""
+        self.processor=""
+    def display(self):
+        print("ram:",self.ram)
+        print("processor:",self.processor)
+
+hp=laptop()
+dell= laptop()
+
+hp.ram="8 gb"
+hp.processor="i9"
+
+dell.ram="16 gb"
+dell.processor="i11"
+
+hp.display()
+dell.display() """
